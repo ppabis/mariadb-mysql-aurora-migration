2e22b79821c1b650ba3b1df1f7d78ff90b4b5d2d
ppabis <piotr@pabis.eu>
2023-11-18 14:40:56 +0100

    Initialized repo

---
+++ README.md
@@ -0,0 +1,4 @@
+Migrating MariaDB to Aurora
+==========================
+This is an example repo where the task is to migrate existing MariaDB RDS
+database to Aurora.
---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.22.0"
+    }
+  }
+  required_version = "~> 1.4"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
