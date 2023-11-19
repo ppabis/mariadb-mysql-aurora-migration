ab12972458a1a9167983c98cc614c9c9e11101fb
ppabis <piotr@pabis.eu>
2023-11-19 18:06:02 +0100

    Add also Null and HTTP providers

--- main.tf
+++ main.tf
@@ -9,6 +9,16 @@ terraform {
       source  = "hashicorp/random"
       version = ">= 3.5.1"
     }
+
+    http = {
+      source  = "hashicorp/http"
+      version = "3.4.0"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.1"
+    }
   }
   required_version = "~> 1.4"
 }
